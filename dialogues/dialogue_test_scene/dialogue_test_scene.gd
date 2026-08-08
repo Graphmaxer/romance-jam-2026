07df5325efa74a70a04b07e0bdae928e935ebe7f
Graphmaxer <16637307+Graphmaxer@users.noreply.github.com>
2026-08-08 23:20:06 +0200

    feat: update character card

--- dialogues/dialogue_test_scene/dialogue_test_scene.gd
+++ dialogues/dialogue_test_scene/dialogue_test_scene.gd
@@ -4,6 +4,7 @@ extends BaseDialogueTestScene
 
 
 func _ready() -> void:
+	GameState.pseudo = "Protagoniste"
 	var viewport_rect: Rect2 = get_viewport_rect()
 	discussion.size = viewport_rect.size
 	discussion.start(resource, title if not title.is_empty() else resource.first_title)
